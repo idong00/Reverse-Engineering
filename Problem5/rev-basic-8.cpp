fb7a69bff7763dbadaeecb18d13d390306c94767
Donghwan Kim <55074166+idong00@users.noreply.github.com>
2024-03-03 14:12:52 +0900

    Changed variable name.

--- Problem5/rev-basic-8.cpp
+++ Problem5/rev-basic-8.cpp
@@ -5,8 +5,8 @@ constexpr unsigned char MIN_CHAR_VALUE = 32;
 constexpr unsigned char MAX_CHAR_VALUE = 127;
 
 /* Array of value to compare */
-constexpr size_t VALUE_TO_COMPARE_ARRAY_SIZE = 21;
-constexpr unsigned char GValueToCompareArray[VALUE_TO_COMPARE_ARRAY_SIZE] =
+constexpr size_t GVALUE_TO_COMPARE_ARRAY_SIZE = 21;
+constexpr unsigned char GVALUE_TO_COMPARE_ARRAY[GVALUE_TO_COMPARE_ARRAY_SIZE] =
 {
 	0xAC, 0xF3, 0x0C, 0x25, 0xA3,
 	0x10, 0xB7, 0x25, 0x16, 0xC6,
@@ -17,13 +17,13 @@ constexpr unsigned char GValueToCompareArray[VALUE_TO_COMPARE_ARRAY_SIZE] =
 
 int main()
 {
-	for (size_t ArrayIdx = 0; ArrayIdx < VALUE_TO_COMPARE_ARRAY_SIZE; ++ArrayIdx)
+	for (size_t ArrayIdx = 0; ArrayIdx < GVALUE_TO_COMPARE_ARRAY_SIZE; ++ArrayIdx)
 	{
 		for (unsigned char Char = MIN_CHAR_VALUE; Char <= MAX_CHAR_VALUE; ++Char)
 		{
 			const unsigned char MulValue = 0xFB * Char;
 
-			if (GValueToCompareArray[ArrayIdx] == (MulValue & 0xFF))
+			if (GVALUE_TO_COMPARE_ARRAY[ArrayIdx] == (MulValue & 0xFF))
 			{
 				printf("%c", Char);
 
@@ -31,7 +31,7 @@ int main()
 			}
 		}
 	}
-	
+
 	return 0;
 }
 
