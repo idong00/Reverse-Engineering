a5467b69cec71a04b646661198d64947e89a8bb3
Donghwan Kim <55074166+idong00@users.noreply.github.com>
2024-03-11 15:02:48 +0900

    Modified variable name.

--- Problem15/rev-basic-5.cpp
+++ Problem15/rev-basic-5.cpp
@@ -7,7 +7,7 @@ constexpr unsigned char GMAX_CHAR_VALUE = 127;
 
 /* 0x7FF6780F3000 */
 constexpr size_t GMAX_LOOP_COUNT = 0x18;
-constexpr unsigned char G0x7FF6DAFB3000[GMAX_LOOP_COUNT] =
+constexpr unsigned char G0x7FF685631000[GMAX_LOOP_COUNT] =
 {
 	0xAD, 0xD8, 0xCB, 0xCB,
 	0x9D, 0x97, 0xCB, 0xC4,
@@ -21,13 +21,13 @@ int main()
 {
 	unsigned char Answer[GMAX_LOOP_COUNT] = { 0, };
 	Answer[GMAX_LOOP_COUNT - 1] = '\0';
-	Answer[GMAX_LOOP_COUNT - 2] = G0x7FF6DAFB3000[GMAX_LOOP_COUNT - 2];
+	Answer[GMAX_LOOP_COUNT - 2] = G0x7FF685631000[GMAX_LOOP_COUNT - 2];
 
 	for (size_t LoopCount = GMAX_LOOP_COUNT - 3; LoopCount >= 0; --LoopCount)
 	{
 		for (unsigned char Char = GMIN_CHAR_VALUE; Char <= GMAX_CHAR_VALUE; ++Char)
 		{
-			if (G0x7FF6DAFB3000[LoopCount] == Char + Answer[LoopCount + 1])
+			if (G0x7FF685631000[LoopCount] == Char + Answer[LoopCount + 1])
 			{
 				Answer[LoopCount] = Char;
 
